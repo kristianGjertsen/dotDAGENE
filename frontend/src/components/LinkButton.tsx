749167100552a33a82c9a43d99137301ac99a244
Kristian Gjertsen <kristiangjertsen5@gmail.com>
2025-11-14 10:26:55 +0100

    FAQ side lagt til

--- frontend/src/components/LinkButton.tsx
+++ frontend/src/components/LinkButton.tsx
@@ -8,17 +8,22 @@ interface ButtonProps {
 }
 
 export const LinkButton = ({ children, link, color }: ButtonProps) => {
+  var textColorClass = "";
+  if (color === 'white' || color === 'yellow') {
+    textColorClass = 'text-black'
+  }
+  else {
+    textColorClass = 'text-white';
+  }
   return (
     <a
       href={link}
       className="group relative inline-block cursor-pointer select-none focus:outline-none whitespace-nowrap"
     >
-      {/* Bottom "shadow" chip */}
       <span aria-hidden="true" className="absolute inset-0 bg-black" />
-      {/* Top actual button chip */}
       <span
         className={
-          'relative block border-3 border-black px-6 py-3 text-center text-2xl font-medium text-white transition-transform duration-150 ' +
+          `relative block border-3 border-black px-6 py-3 text-center text-2xl font-medium ${textColorClass} transition-transform duration-150 ` +
           'group-hover:translate-x-1 group-hover:translate-y-1' +
           colorMap.get(color)
         }
