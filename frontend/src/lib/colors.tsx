b6d07fa19661362d339f2e9292bb04e997b569b5
Kristian Gjertsen <kristiangjertsen@gmail.com>
2025-11-26 00:11:41 +0100

    Start adventskalender

--- frontend/src/lib/colors.tsx
+++ frontend/src/lib/colors.tsx
@@ -1,8 +1,9 @@
-export type color = 'purple' | 'green' | 'yellow' | 'white';
+export type color = 'purple' | 'green' | 'yellow' | 'white' | 'red';
 
 export const colorMap = new Map<color, string>([
   ['green', ' bg-dotgreen'],
   ['purple', ' bg-dotpurple'],
   ['yellow', ' bg-dotyellow'],
-  ['white', ' bg-white']
+  ['white', ' bg-white'],
+  ['red', ' bg-red-500'],
 ]);
