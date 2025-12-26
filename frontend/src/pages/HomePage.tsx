df375f446f7c3cf54ae249cf2988d55eee10c930
Kristian Gjertsen <kristiangjertsen@gmail.com>
2025-12-26 18:43:45 +0100

    Import ikke brukt fjernet

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import {
   BuildingOffice2Icon, SparklesIcon, UserGroupIcon,
 } from '@heroicons/react/20/solid';
